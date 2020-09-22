abe5d88bb8fb9e7d6326c0a33babd97316e22660
Roger Prats <rogerpl@ext.bershka.com>
2020-09-22 18:56:27 +0200

    Initial Commit

---
+++ PracticeTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeTests
+//
+//  Created by Roger Prats Llivina on 22/09/2020.
+//  Copyright © 2020 roger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
